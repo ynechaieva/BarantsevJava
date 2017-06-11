f9f2187b5b93146ca6e9a567851c142a65b65f4a
ynechaieva <ynechaieva@outlook.com>
2017-06-11 20:05:23 +0200

    Project moved to the package pl.pft.sandbox

--- sandbox/src/main/java/pl/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/pl/pft/sandbox/MyFirstProgram.java
@@ -1,6 +1,8 @@
+package pl.pft.sandbox;
+
 public class MyFirstProgram{
 
-	public static void main(String args[]){
+	public static void main(String[] args){
 
 		System.out.println("Hello, world!");
 	}
