11f12754841d4bc91486dbaa9e27d12811821e45
ynechaieva <ynechaieva@outlook.com>
2017-06-13 23:07:11 +0200

    Задание №3: Подключить тестовый фреймворк и сделать первые тесты

--- sandbox/src/main/java/pl/pft/sandbox/DistanceHelper.java
+++ sandbox/src/main/java/pl/pft/sandbox/DistanceHelper.java
@@ -8,21 +8,11 @@ public class DistanceHelper {
 		Point p1 = new Point(3, 8.3);
 		Point p2 = new Point(2.4, 6.1);
 
-		System.out.print("Расстояние между точками (static function) ");
-		p1.print();
-		System.out.print(" и ");
-		p2.print();
-		System.out.print(" = " + distance(p1, p2));
-		System.out.println();
-
-		System.out.print("Расстояние между точками (Point method) ");
+		System.out.print("Расстояние между точками ");
 		p1.print();
 		System.out.print(" и ");
 		p2.print();
 		System.out.print(" = " + p1.distance(p2));
-	}
-
-	public static double distance(Point p1, Point p2){
-		return Math.sqrt(Math.pow((p1.x - p2.x), 2) + Math.pow((p1.y - p2.y), 2));
+		System.out.println();
 	}
 }
---
+++ sandbox/src/test/java/pl/pft/sandbox/PointTests.java
@@ -0,0 +1,46 @@
+package pl.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Created by ynech on 13/06/2017.
+ */
+public class PointTests {
+
+  @Test
+  public void testIdenticalPoints(){
+    Point pt1 = new Point(1.0, 2.0);
+    Point pt2 = new Point(1.0, 2.0);
+
+    //assert pt1.distance(pt2) == 0.0;
+    Assert.assertEquals(pt1.distance(pt2), 0.0);
+  }
+
+  @Test
+  public void testPositivePoints(){
+    Point pt1 = new Point(2.7, 3.2);
+    Point pt2 = new Point(1.3, 4.5);
+
+    //assert pt1.distance(pt2) == 0.0;
+    Assert.assertEquals(pt1.distance(pt2), 1.91049731745428);
+  }
+
+  @Test
+  public void testNegativePoints(){
+    Point pt1 = new Point(-2.7, -3.2);
+    Point pt2 = new Point(-1.3, -4.5);
+
+    //assert pt1.distance(pt2) == 0.0;
+    Assert.assertEquals(pt1.distance(pt2), 1.91049731745428);
+  }
+
+  @Test
+  public void testDifferentPoints(){
+    Point pt1 = new Point(-2.7, 3.2);
+    Point pt2 = new Point(1.3, -4.5);
+
+    //assert pt1.distance(pt2) == 0.0;
+    Assert.assertEquals(pt1.distance(pt2), 8.676981041814026);
+  }
+}
