568f3cd2b2f430fabe0dc813c942cc79c327a4d2
ynechaieva <ynechaieva@outlook.com>
2017-06-19 21:17:28 +0200

    Задание №4: Сделать тесты для создания групп и контактов в адресной книге

---
+++ addressbook-web-tests/src/test/java/pl/pft/addressbook/ContactCreationTest.java
@@ -0,0 +1,75 @@
+package pl.pft.addressbook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ynech on 18/06/2017.
+ */
+public class ContactCreationTest {
+  FirefoxDriver wd;
+
+  @BeforeMethod
+  public void setUp() throws Exception {
+    wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
+    wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+
+    login("admin", "secret");
+  }
+
+  private void login(String username, String password) {
+    wd.get("http://localhost/addressbook/");
+    wd.findElement(By.cssSelector("html")).click();
+    wd.findElement(By.name("user")).click();
+    wd.findElement(By.name("user")).clear();
+    wd.findElement(By.name("user")).sendKeys(username);
+    wd.findElement(By.name("pass")).click();
+    wd.findElement(By.name("pass")).clear();
+    wd.findElement(By.name("pass")).sendKeys(password);
+    wd.findElement(By.xpath("//form[@id='LoginForm']/input[3]")).click();
+  }
+
+  @Test
+  public void testContactCreation(){
+    gotoContactPage();
+    fillContactForm(new ContactData("testUser1_FN", "testUser1_LN", "testUser1@email.address"));
+    submitContactCreation();
+    gotoHomePage();
+
+  }
+
+  private void gotoContactPage(){
+    wd.findElement(By.linkText("ADD_NEW")).click();
+  }
+
+  private void fillContactForm(ContactData contact){
+    wd.findElement(By.name("firstname")).click();
+    wd.findElement(By.name("firstname")).clear();
+    wd.findElement(By.name("firstname")).sendKeys(contact.getFirstName());
+    wd.findElement(By.name("lastname")).click();
+    wd.findElement(By.name("lastname")).clear();
+    wd.findElement(By.name("lastname")).sendKeys(contact.getLastName());
+    wd.findElement(By.name("email")).click();
+    wd.findElement(By.name("email")).clear();
+    wd.findElement(By.name("email")).sendKeys(contact.getEmail());
+  }
+
+  private void submitContactCreation(){
+    wd.findElement(By.name("submit")).click();
+  }
+
+  private void gotoHomePage(){
+    wd.findElement(By.linkText("HOME")).click();
+  }
+
+  @AfterMethod
+  public void tearDown() {
+    wd.quit();
+  }
+}
---
+++ addressbook-web-tests/src/test/java/pl/pft/addressbook/ContactData.java
@@ -0,0 +1,122 @@
+package pl.pft.addressbook;
+
+/**
+ * Created by ynech on 18/06/2017.
+ */
+public class ContactData {
+  private String firstName;
+  private String middleName;
+  private String lastName;
+  private String nickname;
+  private String title;
+  private String company;
+  private String address;
+  private String homePhone;
+  private String mobilePhone;
+  private String workPhone;
+  private String fax;
+  private String email;
+  private String email2;
+  private String email3;
+  private String homepage;
+  private int bDay;
+  private int bMonth;
+  private int bYear;
+
+  public ContactData(String firstName, String lastName, String email){
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.email = email;
+  }
+
+  public void setFirstName(String firstName){
+    this.firstName = firstName;
+  }
+
+  public void setMiddleName(String middleName){
+    this.middleName = middleName;
+  }
+
+  public void setLastName(String lastName){
+    this.lastName = lastName;
+  }
+
+  public void setNickname(String nickname){
+    this.nickname = nickname;
+  }
+
+  public void setTitle(String title){
+    this.title = title;
+  }
+
+  public void setCompany(String company){
+    this.company = company;
+  }
+
+  public void setAddress(String address){
+    this.address = address;
+  }
+
+  public void setHomePhone(String homePhone){
+    this.homePhone = homePhone;
+  }
+
+  public void setMobilePhone(String mobilePhone){
+    this.mobilePhone = mobilePhone;
+  }
+
+  public void setWorkPhone(String workPhone){
+    this.workPhone = workPhone;
+  }
+
+  public void setFax(String fax){
+    this.fax = fax;
+  }
+
+  public void setEmail(String email){
+    this.email = email;
+  }
+
+  public void setEmail2(String email2){
+    this.email2 = email2;
+  }
+
+  public void setEmail3(String email3){
+    this.email3 = email3;
+  }
+
+  public void setHomepage(String homepage){
+    this.homepage = homepage;
+  }
+
+  public void setbDay(int bDay){
+    this.bDay = bDay;
+  }
+
+  public void setbMonth(int bMonth){
+    this.bMonth = bMonth;
+  }
+
+  public void setbYear(int bYear){
+    this.bYear = bYear;
+  }
+
+  public String getFirstName(){return this.firstName;}
+  public String getMiddleName(){return this.middleName;}
+  public String getLastName(){return this.lastName;}
+  public String getNickname(){return this.nickname;}
+  public String getTitle(){return this.title;}
+  public String getCompany(){return this.company;}
+  public String getAddress(){return this.address;}
+  public String getHomePhone(){return this.homePhone;}
+  public String getMobilePhone(){return this.mobilePhone;}
+  public String getWorkPhone(){return this.workPhone;}
+  public String getFax(){return this.fax;}
+  public String getEmail(){return this.email;}
+  public String getEmail2(){return this.email2;}
+  public String getEmail3(){return this.email3;}
+  public String getHomepage(){return this.homepage;}
+  public int getbDay(){return this.bDay;}
+  public int getbMonth(){return this.bMonth;}
+  public int getbYear(){return this.bYear;}
+}
