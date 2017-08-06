b0f2495c3b7edaa2d24e53d3a7764043da020ab6
ynechaieva <ynechaieva@outlook.com>
2017-08-06 15:35:39 +0200

    LoginTests class is created

---
+++ mantis-tests/src/test/java/pl/pft/mantis/tests/LoginTests.java
@@ -0,0 +1,19 @@
+package pl.pft.mantis.tests;
+
+
+import org.testng.annotations.Test;
+import pl.pft.mantis.appmanager.HttpSession;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends TestBase {
+
+  @Test
+  public void testLogin() throws IOException {
+    HttpSession session = app.newSession();
+    assertTrue(session.login("administrator", "root"));
+    assertTrue(session.isLoggedInAs("administrator"));
+  }
+}
